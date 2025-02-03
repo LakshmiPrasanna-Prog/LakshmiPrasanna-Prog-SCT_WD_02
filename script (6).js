304c121fa85d878da63b898fe754da5334ed7506
LakshmiPrasanna-Prog <upputalalakshmiprasanna@gmail.com>
2025-02-03 19:27:04 +0530

    Add files via upload

---
+++ script (6).js
@@ -0,0 +1,69 @@
+var ms = 0, s = 0, m = 0, h = 0;
+var timer = null;
+
+var display = document.querySelector(".timer-Display");
+var laps = document.querySelector(".laps");
+
+function start() {
+    if (!timer) {
+        timer = setInterval(run, 10);
+    }
+}
+
+function run() {
+    display.innerHTML = getTimer();
+    ms++;              
+    if (ms == 100) {
+        ms = 0;
+        s++;
+    }
+    if (s == 60) {
+        s = 0;
+        m++;
+    }
+    if (m == 60) {
+        m = 0;
+        h++;
+    }
+}
+
+function getTimer() {
+    return (h < 10 ? "0" + h : h) + " : " + 
+           (m < 10 ? "0" + m : m) + " : " + 
+           (s < 10 ? "0" + s : s) + " : " + 
+           (ms < 10 ? "0" + ms : ms);
+}
+
+function pause() {
+    if (timer) {
+        clearInterval(timer);
+        timer = null; // … Ensure the timer is properly stopped
+    }
+}
+
+function reset() {
+    pause();
+    ms = 0;
+    s = 0;
+    m = 0;
+    h = 0;
+    display.innerHTML = getTimer();
+}
+
+function restart() {
+    reset(); 
+    start();
+}
+
+// Lap - capturing current time
+function lap() {
+    if (timer) {   
+        var Li = document.createElement("li");
+        Li.innerHTML = getTimer();
+        laps.appendChild(Li);
+    }
+}
+
+function resetLap() {
+    laps.innerHTML = "";
+}
